e5bf09f9f0f9657b74959c016fdd1b4dfcbd7ee7
Hector Soto <hectors.bts@gmail.com>
2019-10-02 16:13:57 -0500

    Heroku

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
-app.use(bodyParser.urlencoded({extended:false}));
+const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(port, ()=>{
-    console.log("Se armó ne 3000");
+app.listen(port || 4000, () => {
+  console.log(`Node server running on ${port}`);
 });
