4994e593bef754849a7ebc66953213002c47904c
Hector Soto <hectors.bts@gmail.com>
2019-10-02 17:04:19 -0500

    Serve image

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// get images
+app.get('/assets/teamlogos/:img', (req, res) => {
+  res.sendFile(path.join(__dirname, 'assets/teamlogos', req.params.img));
+});
+
 app.listen(port || 4000, () => {
   console.log(`Node server running on ${port}`);
 });
