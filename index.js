8dbac74d3c463580665997b20cc6f1cb70ff973f
unknown <cristianliljon@hotmail.com>
2019-10-02 22:20:35 -0500

    sign up creado

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pool = require("./db_pool");
+const sign_up = require('./sign_up');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = process.env.PORT;
@@ -24,6 +26,31 @@ app.get('/', (req, res) => {
       res.send(rows);
     });
   });
+});
+
+app.post('/users',(req, res)=>{
+  const body = req.body;
+  res.set("content-type","application/json")
+  if(body.password === body.confirm_password){
+    bcrypt.hash(body.password, 5)
+    .then(hash=>{
+      sign_up(body.name, body.lastName, body.email, body.password)
+      .then((result)=>{
+        res.status(200);
+        res.send("Created");
+      })
+      .catch((err)=>{
+        res.status(400);
+        res.send("Error: " + JSON.stringify(err));
+      });
+    }).catch((e)=>{
+      res.status(500);
+      res.send("Password error: " + e.Message);
+    });
+  }else{
+    res.status(400);
+    res.send("Different password");
+  }
 });
 
 // get images
