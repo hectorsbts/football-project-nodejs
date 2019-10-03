86736bcd385b6a7ec2656b740e10b5d1a51608dc
Hector Soto <hectors.bts@gmail.com>
2019-10-02 21:21:54 -0500

    Removed football data helper functions

--- index.js
+++ index.js
@@ -43,19 +43,6 @@ app.post('/teams', (req, res) => {
     .catch((err) => res.send(err));
 });
 
-// add scores
-app.post('/scores', (req, res) => {
-  res.set({
-    'Content-Type': 'application/json',    
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  });
-
-  addScores(req.body)
-    .then((result) => res.send(result))
-    .catch((err) => res.send(err));
-});
-
 // get images
 app.get('/assets/teamlogos/:img', (req, res) => {
   res.sendFile(path.join(__dirname, 'assets/teamlogos', req.params.img));
